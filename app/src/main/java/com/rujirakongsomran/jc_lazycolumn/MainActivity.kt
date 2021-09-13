a04dd3b0f22ae7ba1b16845390f6ada28759c7c7
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-13 13:33:37 +0700

    Sticky Header

--- app/src/main/java/com/rujirakongsomran/jc_lazycolumn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_lazycolumn/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -13,33 +17,43 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_lazycolumn.repository.PersonRepository
 import com.rujirakongsomran.jc_lazycolumn.ui.theme.JC_LazyColumnTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JC_LazyColumnTheme {
 
-                val personRepository = PersonRepository()
-                val getAllData = personRepository.getAllData()
-
+                val sections = listOf("A", "B", "C", "D", "E", "F", "G")
                 LazyColumn(
                     contentPadding = PaddingValues(all = 12.dp),
                     verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    itemsIndexed(
-                        items = getAllData,
-                        key = { index, person ->
-                            person.id
+                    sections.forEach { section ->
+                        stickyHeader {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.LightGray)
+                                    .padding(12.dp),
+                                text = "Section $section"
+                            )
                         }
-                    ) { index, person ->
-                        Log.d("MainActivity", index.toString())
-                        CustomItem(person = person)
+                        items(10) {
+                            Text(
+                                text = "Item $it from the section $section"
+                            )
+                        }
+
                     }
+
                 }
             }
         }
